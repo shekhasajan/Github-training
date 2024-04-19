acdb05b28d8a1a36d3ebd2f59478fe5c3f8ab362
shekhasajan <shekha@jobinandjismi.com>
2024-04-19 11:40:43 +0530

    OTP-6758-ci:github-training

---
+++ Taining-git/src/FileCabinet/SuiteScripts/JobinAndJismi/github-sample.js
@@ -0,0 +1,201 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType ClientScript
+ * @NModuleScope SameAccount
+ */
+define(["N/search"], /**
+ * @param{search} search
+ */
+function (search) {
+  /**
+   * Function to be executed after page is initialized.
+   *
+   * @param {Object} scriptContext
+   * @param {Record} scriptContext.currentRecord - Current form record
+   * @param {string} scriptContext.mode - The mode in which the record is being accessed (create, copy, or edit)
+   *
+   * @since 2015.2
+   */
+  function pageInit(scriptContext) {
+    
+    let isSavedSearchCreated = checkSavedSearch();
+console.log('isSavedSearchCreated', isSavedSearchCreated);
+if (isSavedSearchCreated==0){
+    let prevMonthCusSearch2 = search.create({
+      type: "customer",
+      title: "Prev month customer Search2",
+      id: "customsearch_my_so_prevp_search2",
+      filters: [
+        ["datecreated", "within", "lastmonth"],
+        "AND",
+        ["subsidiary", "anyof", "11"],
+      ],
+      columns: [
+        search.createColumn({ name: "entityid", label: "Name" }),
+        search.createColumn({ name: "email", label: "Email" }),
+        search.createColumn({
+          name: "subsidiary",
+          label: "Primary Subsidiary",
+        }),
+        search.createColumn({ name: "salesrep", label: "Sales Rep" }),
+        search.createColumn({ name: "datecreated", label: "Date Created" }),
+      ],
+    });
+
+
+    let myResultSet = prevMonthCusSearch2.run();
+    console.log("myResultSet", myResultSet);
+    let searchResultCount = prevMonthCusSearch2.runPaged().count;
+    log.debug("customerSearchObj result count", searchResultCount);
+    prevMonthCusSearch2.run().each(function (result) {
+      // .run().each has a limit of 4,000 results
+      return true;
+    });
+
+    prevMonthCusSearch2.save();
+}
+  }
+function checkSavedSearch(){
+    var savedsearchSearchObj = search.create({
+        type: "savedsearch",
+        filters:
+        [
+           ["id","is","customsearch_my_so_prevp_search2"]
+        ]
+     });
+     var searchResultCount = savedsearchSearchObj.runPaged().count;
+     log.debug("savedsearchSearchObj result count",searchResultCount);
+     return searchResultCount;
+   
+     
+  
+}
+ 
+
+  /**
+   * Function to be executed when field is changed.
+   *
+   * @param {Object} scriptContext
+   * @param {Record} scriptContext.currentRecord - Current form record
+   * @param {string} scriptContext.sublistId - Sublist name
+   * @param {string} scriptContext.fieldId - Field name
+   * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
+   * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
+   *
+   * @since 2015.2
+   */
+  function fieldChanged(scriptContext) {}
+
+  /**
+   * Function to be executed when field is slaved.
+   *
+   * @param {Object} scriptContext
+   * @param {Record} scriptContext.currentRecord - Current form record
+   * @param {string} scriptContext.sublistId - Sublist name
+   * @param {string} scriptContext.fieldId - Field name
+   *
+   * @since 2015.2
+   */
+  function postSourcing(scriptContext) {}
+
+  /**
+   * Function to be executed after sublist is inserted, removed, or edited.
+   *
+   * @param {Object} scriptContext
+   * @param {Record} scriptContext.currentRecord - Current form record
+   * @param {string} scriptContext.sublistId - Sublist name
+   *
+   * @since 2015.2
+   */
+  function sublistChanged(scriptContext) {}
+
+  /**
+   * Function to be executed after line is selected.
+   *
+   * @param {Object} scriptContext
+   * @param {Record} scriptContext.currentRecord - Current form record
+   * @param {string} scriptContext.sublistId - Sublist name
+   *
+   * @since 2015.2
+   */
+  function lineInit(scriptContext) {}
+
+  /**
+   * Validation function to be executed when field is changed.
+   *
+   * @param {Object} scriptContext
+   * @param {Record} scriptContext.currentRecord - Current form record
+   * @param {string} scriptContext.sublistId - Sublist name
+   * @param {string} scriptContext.fieldId - Field name
+   * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
+   * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
+   *
+   * @returns {boolean} Return true if field is valid
+   *
+   * @since 2015.2
+   */
+  function validateField(scriptContext) {}
+
+  /**
+   * Validation function to be executed when sublist line is committed.
+   *
+   * @param {Object} scriptContext
+   * @param {Record} scriptContext.currentRecord - Current form record
+   * @param {string} scriptContext.sublistId - Sublist name
+   *
+   * @returns {boolean} Return true if sublist line is valid
+   *
+   * @since 2015.2
+   */
+  function validateLine(scriptContext) {}
+
+  /**
+   * Validation function to be executed when sublist line is inserted.
+   *
+   * @param {Object} scriptContext
+   * @param {Record} scriptContext.currentRecord - Current form record
+   * @param {string} scriptContext.sublistId - Sublist name
+   *
+   * @returns {boolean} Return true if sublist line is valid
+   *
+   * @since 2015.2
+   */
+  function validateInsert(scriptContext) {}
+
+  /**
+   * Validation function to be executed when record is deleted.
+   *
+   * @param {Object} scriptContext
+   * @param {Record} scriptContext.currentRecord - Current form record
+   * @param {string} scriptContext.sublistId - Sublist name
+   *
+   * @returns {boolean} Return true if sublist line is valid
+   *
+   * @since 2015.2
+   */
+  function validateDelete(scriptContext) {}
+
+  /**
+   * Validation function to be executed when record is saved.
+   *
+   * @param {Object} scriptContext
+   * @param {Record} scriptContext.currentRecord - Current form record
+   * @returns {boolean} Return true if record is valid
+   *
+   * @since 2015.2
+   */
+  function saveRecord(scriptContext) {}
+
+  return {
+    pageInit: pageInit,
+    fieldChanged: fieldChanged,
+    postSourcing: postSourcing,
+    sublistChanged: sublistChanged,
+    lineInit: lineInit,
+    validateField: validateField,
+    validateLine: validateLine,
+    validateInsert: validateInsert,
+    validateDelete: validateDelete,
+    saveRecord: saveRecord,
+  };
+});
